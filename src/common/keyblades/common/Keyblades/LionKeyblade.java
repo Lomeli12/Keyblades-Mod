9b9a55956ffa3aac56835e4db5cf0e87c0be1160
Anthony Lomeli <anthonylomeli11@gmail.com>
2012-10-11 15:41:20 -0700

    Update src/common/keyblades/common/Keyblades/LionKeyblade.java

--- src/common/keyblades/common/Keyblades/LionKeyblade.java
+++ src/common/keyblades/common/Keyblades/LionKeyblade.java
@@ -12,7 +12,7 @@ public class LionKeyblade extends ItemSword{
 		return "/Keyblademod/keyblades.png";
 	}
 	public ItemStack onItemRightClick(ItemStack itemstack, World world, EntityPlayer entityplayer)
-    {
+	{
 		itemstack.damageItem(1, entityplayer);
 		world.playSoundAtEntity(entityplayer, "random.bow", 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
 		if (!world.isRemote)
@@ -20,13 +20,13 @@ public class LionKeyblade extends ItemSword{
 			Vec3 look = entityplayer.getLookVec();
 			EntitySmallFireball fireball = new EntitySmallFireball(world, entityplayer, 0, 0, 0);
 			fireball.setPosition(entityplayer.posX + look.xCoord * 2, entityplayer.posY + look.yCoord + 1, entityplayer.posZ + look.zCoord * 2);
-            fireball.accelerationX = look.xCoord * 0.1;
-            fireball.accelerationY = look.yCoord * 0.1;
-            fireball.accelerationZ = look.zCoord * 0.1;
-            world.spawnEntityInWorld(fireball);
-            }
+            		fireball.accelerationX = look.xCoord * 0.1;
+            		fireball.accelerationY = look.yCoord * 0.1;
+            		fireball.accelerationZ = look.zCoord * 0.1;
+            		world.spawnEntityInWorld(fireball);
+            	}
 		return itemstack;
-    }
+	}
 	public boolean hitEntity(ItemStack items, EntityLiving Living1, EntityLiving Living2)
 	{
 		Living1.setFire(5);
