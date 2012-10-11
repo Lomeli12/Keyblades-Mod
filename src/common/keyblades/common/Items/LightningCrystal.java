8ac50799a3ff556156922d83910e9a51b4ffc1af
Anthony Lomeli <anthonylomeli11@gmail.com>
2012-10-11 15:10:36 -0700

    Update src/common/keyblades/common/Items/LightningCrystal.java
    fixed the spacing that was bugging me.

--- src/common/keyblades/common/Items/LightningCrystal.java
+++ src/common/keyblades/common/Items/LightningCrystal.java
@@ -37,27 +37,27 @@ public class LightningCrystal extends Item {
 		double d2 = entityplayer.prevPosZ + (entityplayer.posZ - entityplayer.prevPosZ) * (double)f;
 		Vec3 vec3d = Vec3.createVectorHelper(d, d1, d2);
 		float f3 = MathHelper.cos(-f2 * 0.01745329F - 3.141593F);
-		 float f4 = MathHelper.sin(-f2 * 0.01745329F - 3.141593F);
-		 float f5 = -MathHelper.cos(-f1 * 0.01745329F);
-		 float f6 = MathHelper.sin(-f1 * 0.01745329F);
-		 float f7 = f4 * f5;
-		 float f8 = f6;
-		 float f9 = f3 * f5;
-		 double d3 = 5000D;
-		 Vec3 vec3d1 = vec3d.addVector((double)f7 * d3, (double)f8 * d3, (double)f9 * d3);
-		 MovingObjectPosition movingobjectposition = world.rayTraceBlocks_do_do(vec3d, vec3d1, false, true);
-		 if (movingobjectposition == null) 
-		 {
-			 return itemstack;
-		 }
-		 if (movingobjectposition.typeOfHit == EnumMovingObjectType.TILE) 
-		 {
-			 int i = movingobjectposition.blockX;
-			 int j = movingobjectposition.blockY;
-			 int k = movingobjectposition.blockZ;
-			 world.spawnEntityInWorld(new EntityLightningBolt(world, i, j, k));
-	     }
-		 	itemstack.damageItem(1, entityplayer);
+		float f4 = MathHelper.sin(-f2 * 0.01745329F - 3.141593F);
+		float f5 = -MathHelper.cos(-f1 * 0.01745329F);
+		float f6 = MathHelper.sin(-f1 * 0.01745329F);
+		float f7 = f4 * f5;
+		float f8 = f6;
+		float f9 = f3 * f5;
+		double d3 = 5000D;
+		Vec3 vec3d1 = vec3d.addVector((double)f7 * d3, (double)f8 * d3, (double)f9 * d3);
+		MovingObjectPosition movingobjectposition = world.rayTraceBlocks_do_do(vec3d, vec3d1, false, true);
+		if (movingobjectposition == null) 
+		{
+			return itemstack;
+		}
+		if (movingobjectposition.typeOfHit == EnumMovingObjectType.TILE) 
+		{
+			int i = movingobjectposition.blockX;
+			int j = movingobjectposition.blockY;
+			int k = movingobjectposition.blockZ;
+			world.spawnEntityInWorld(new EntityLightningBolt(world, i, j, k));
+	     	}
+		itemstack.damageItem(1, entityplayer);
 	        return itemstack;
-	    }
+	}
 }
